e7b16be02ab02b0163cb5b652a1ce214c15ae1e7
subhra <subhra@kali>
2025-06-20 12:15:42 +0530

    server run successfully

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-
-const app = express()
+import userRouter from './routes/user.routes.js'
+const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
+
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser()) 
+
+//routes import 
+
+
+//routes declaration
+console.log("✅ User routes mounted at /api/v1/users");
+app.use("/api/v1/users", userRouter)
 
 
 export { app }
